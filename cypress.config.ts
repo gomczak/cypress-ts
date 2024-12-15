a365d123045c3404a50df586940a1869f6d3a92a
Małgorzata Tomczak <tomczakmalgorzata2@gmail.com>
2024-12-15 11:11:35 +0100

    fixed: config and path name

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,23 @@
+import { defineConfig } from "cypress";
 import { allureCypress } from "allure-cypress/reporter";
 
-export default {
+export default defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:8081',
+    env: {
+      backendUrl: 'http://localhost:4001'
+    },
     setupNodeEvents(on, config) {
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.family === 'chromium' && browser.name !== 'electron') {
+          launchOptions.args.push('--disable-search-engine-choice-screen')
+        }
+        return launchOptions
+      })
       allureCypress(on, config, {
         resultsDir: "allure-results",
       });
       return config;
     },
   },
-};
+});
