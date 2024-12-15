ec31b35cd40f936a72d262739eaa91a17a3c93a2
Małgorzata Tomczak <tomczakmalgorzata2@gmail.com>
2024-12-15 10:55:01 +0100

    added allure report

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,18 +1,12 @@
-import { defineConfig } from "cypress";
+import { allureCypress } from "allure-cypress/reporter";
 
-export default defineConfig({
+export default {
   e2e: {
-    baseUrl: 'http://localhost:8081',
-    env: {
-      backendUrl: 'http://localhost:4001'
-    },
     setupNodeEvents(on, config) {
-      on('before:browser:launch', (browser, launchOptions) => {
-        if (browser.family === 'chromium' && browser.name !== 'electron') {
-          launchOptions.args.push('--disable-search-engine-choice-screen')
-        }
-        return launchOptions
-      })
+      allureCypress(on, config, {
+        resultsDir: "allure-results",
+      });
+      return config;
     },
   },
-});
+};
